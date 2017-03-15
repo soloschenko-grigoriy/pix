cb607c38053ea0ff895961a8d33975367181fe5c
Gregory Soloschenko <soloschenko@gmail.com>
2017-03-15 18:53:45 -0500

    Ship should have health params (285948243452683)

--- src/entities/ship.js
+++ src/entities/ship.js
@@ -1,18 +1,23 @@
 var Sprite          = window.PIXI.Sprite,
     Loader          = window.PIXI.loader,
     Texture         = window.PIXI.Texture,
+    Graphics        = window.PIXI.Graphics,
+    Container       = window.PIXI.Container,
     AnimatedSprite  = window.PIXI.extras.AnimatedSprite;
 
 import Bullet from './bullet';
 
 export default class Ship{
     constructor(params){
-        this._bodyHp    = params.bodyHp || 100;
-        this._sailHp    = params.sailHp || 100;
-        this._bullets   = params.bullets || 0;
-        this._speed     = params.speed || 1;
+        this.bodyHp    = params.bodyHp  || 100;
+        this.sailHp    = params.sailHp  || 100;
+        this.bullets   = params.bullets || 0;
+        this.speed     = params.speed   || 1;
+        
+        this._oldBodyHp = params.bodyHp  || 100;
+        this._currentBodyHp = params.bodyHp  || 100;
 
-        this._stage = params.stage;
+        this.stage = params.stage;
 
         if(params.noAutoRender){
             return;
@@ -63,42 +68,76 @@ export default class Ship{
     }
 
     render(){
+        this.container = new Container();
+        this.container.x = 300;
+        this.container.y = 300;
+        this.container.vx = 0;
+        this.container.vy = 0;
+        // this.container.vr = 0;
+
+        this.elm = this.renderMoving();
+        this.stopped = this.renderStopped();
+       
+        this.isStopping = false;
+        this.isAccelerating = false;
+
+        this.hp = this.renderHP(1);
+
+        this.addListeners();
+
+        this.stage.addChild(this.container);
+        
+        requestAnimationFrame(this.update.bind(this));
+    }
+
+    renderStopped(){
+        let sprite = new Sprite(Loader.resources["assets/img/ship0-stop.png"].texture);
+        sprite.anchor.set(0.5);
+        sprite.scale.set(0.3);
+        sprite.rotation = -Math.PI/3;
+        sprite.visible = true;
+
+        this.container.addChild(sprite);
+
+        return sprite;
+    }
+
+    renderMoving(){
         let frames = [];
         for (let i = 1; i < 15; i++) {
             frames.push(Texture.fromFrame(i + '.png'));
         }
 
-        this.stopped = new Sprite( Loader.resources["assets/img/ship0-stop.png"].texture);
-        this.stopped.anchor.set(0.5);
-        this.stopped.visible = false;
-        this.stopped.scale.x = 0.3;
-        this.stopped.scale.y = 0.3;
-        this.stopped.x = 300;
-        this.stopped.y = 300;
-        this.stopped.visible = true;
-        this._stage.addChild(this.stopped);
+        let sprite = new AnimatedSprite(frames);
+        sprite.anchor.set(0.5);
+        sprite.scale.set(0.3);
+        sprite.gotoAndStop(7);
+        sprite.rotation = -Math.PI/3;
+        sprite.animationSpeed = 0.2;
+        sprite.visible = false;
 
-        this.anim = new AnimatedSprite(frames);
-        this.anim.anchor.set(0.5);
-        this.anim.x = 300;
-        this.anim.y = 300;
-        this.anim.gotoAndStop(7);
-        this.anim.rotation = -Math.PI/3;
-        this.anim.animationSpeed = 0.2;
-        this.anim.scale.x = 0.3;
-        this.anim.scale.y = 0.3;
-        this.anim.visible = false;
-        this._stage.addChild(this.anim);
+        this.container.addChild(sprite);
 
+        return sprite;
+    }
 
-        this.anim.vx = 0;
-        this.anim.vy = 0;
-        this.anim.vr = 0;
-        this.anim.isStopping = false;
-        this.anim.isAccelerating = false;
+    renderHP(){
+        var graphics = this.graphics || new Graphics();
+        var w = 80;
+        var h = 5;
 
-        this.addListeners();
-        requestAnimationFrame(this.update.bind(this));
+        graphics.beginFill(0xffffff);
+        graphics.drawRect(this.elm.x - w/2, this.elm.y - 40, w, h);
+        graphics.endFill();
+
+        graphics.beginFill(0xe74c3c);
+        graphics.drawRect(this.elm.x - w/2, this.elm.y - 40, w * this._currentBodyHp / 100, h);
+        graphics.endFill();
+
+        
+        this.container.addChild(graphics);
+
+        return graphics;
     }
 
     addListeners(){
@@ -147,34 +186,26 @@ export default class Ship{
     }
 
     _rotateLeft(){
-        this.anim.rotation -= 0.05;  
+        this.elm.rotation -= 0.05;  
     }
 
     _rotateRight(){
-        this.anim.rotation += 0.05;
+        this.elm.rotation += 0.05;
         
     }
 
     _rotateUp(){
-        if(this.anim.isAccelerating){ return;}
-        this.anim.isStopping = false;          
-        this.anim.isAccelerating = true;
-        this.anim.vx = 1;
-        this.anim.vy = 1;
+        if(this.isAccelerating){ return;}
+        this.isStopping = false;          
+        this.isAccelerating = true;
+        this.vx = 1;
+        this.vy = 1;
     }
 
     _rotateDown(){
-        if(this.anim.isStopping){return;}
-        this.anim.isAccelerating = false;
-        this.anim.isStopping = true;
-    }
-
-    move(){
-
-    }
-
-    fire(){
-
+        if(this.isStopping){return;}
+        this.isAccelerating = false;
+        this.isStopping = true;
     }
 
     destroy(){
@@ -184,49 +215,58 @@ export default class Ship{
     shoot(){
         new Bullet({
             stage: this.stage,
-            x: this.anim.x,
-            y: this.anim.y,
+            x: this.elm.x,
+            y: this.elm.y,
             endX: 100,
             endY: 100
         });
     }
 
     update(){
-        if(this.anim.isStopping){
-            this.anim.vx -= 0.03;
-            this.anim.vy -= 0.03;
+        if(this.isStopping){
+            this.container.vx -= 0.03;
+            this.container.vy -= 0.03;
         }
         
-        if(this.anim.isAccelerating){
-            if(this.anim.vx < 2){
-                this.anim.vx += 0.01;
+        if(this.isAccelerating){
+            if(this.container.vx < 2){
+                this.container.vx += 0.01;
             }
 
-            if(this.anim.vy < 2){
-                this.anim.vy += 0.01;
+            if(this.container.vy < 2){
+                this.container.vy += 0.01;
             }
         }
 
-        if((this.anim.vx < 0 || this.anim.vy < 0) && this.anim.isStopping){
-            this.anim.vx = 0;
-            this.anim.vy = 0;
-            this.anim.gotoAndStop(7);
-            this.anim.isStopping = false;
-            this.anim.visible = false;
-            this.stopped.x = this.anim.x;
-            this.stopped.y = this.anim.y;
-            this.stopped.rotation = this.anim.rotation;
+        if((this.container.vx < 0 || this.container.vy < 0) && this.isStopping){
+            this.container.vx = 0;
+            this.container.vy = 0;
+            this.elm.gotoAndStop(7);
+            this.isStopping = false;
+            this.elm.visible = false;
+            this.stopped.x = this.elm.x;
+            this.stopped.y = this.elm.y;
+            this.stopped.rotation = this.elm.rotation;
             this.stopped.visible = true;        
-        }else if(!this.anim.playing && this.anim.isAccelerating){
-            this.anim.visible = true;
+        }else if(!this.playing && this.isAccelerating){
+            this.elm.visible = true;
             this.stopped.visible = false; 
-            this.anim.play();
+            this.elm.play();
         }
         
-        this.anim.rotation += this.anim.vr;
-        this.stopped.rotation = this.anim.rotation;
-        this.anim.x = this.anim.x + this.anim.vx * Math.sin(this.anim.rotation);
-        this.anim.y = this.anim.y - this.anim.vy * Math.cos(this.anim.rotation);
+        this.stopped.rotation = this.elm.rotation;
+        this.container.x = this.container.x + this.container.vx * Math.sin(this.elm.rotation);
+        this.container.y = this.container.y - this.container.vy * Math.cos(this.elm.rotation);
+        
+        if(this.bodyHp !== this._currentBodyHp){ // hp been changed
+            if(this.bodyHp < this._currentBodyHp){
+                this._currentBodyHp = this._currentBodyHp - 1;
+            }else{
+                this._currentBodyHp = this._currentBodyHp + 1;
+            }
+            
+            this.renderHP();
+        }
         
         // if(this.anim.x > app.view.width - this.anim.width + 100|| this.anim.x < this.anim.width - 100){
         //     this.anim.vx = 0;
